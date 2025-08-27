10b01ad28bf0dc55b1ea8459d85afbf252bfc276
SANTIAGO2575 <143728516+SANTIAGO2575@users.noreply.github.com>
2025-08-26 22:04:41 -0600

    Add files via upload

---
+++ pag_principal/carrusel.js
@@ -0,0 +1,11 @@
+const form = document.querySelectorAll('.formulario');
+
+const prevBtn = document.getElementById('anterior');
+const nextBtn = document.getElementById('posterior');
+let currentIndex = 0;
+
+// function updateCarrusel(){
+//     form.forEach(formulario, index) => {
+
+//     }
+// }
